9232234699816ae695722aa5b7e4a4f447e4c49e
Breimerct98 <breimer.correa@zabud.com.co>
2022-07-22 22:05:06 -0500

    config 2

--- app/src/main/java/com/parcialfinalapp/MainActivity.java
+++ app/src/main/java/com/parcialfinalapp/MainActivity.java
@@ -3,9 +3,7 @@ package com.parcialfinalapp;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
 import android.os.Bundle;
-
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
